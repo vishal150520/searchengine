e06e4836dd89ea52ed645fc6fa7c96f1a52ebcd5
Amrendra.Singh <amrendra.singh@ihorizons.com>
2022-12-16 16:41:01 +0530

    jkjkj

---
+++ frameworks.php
@@ -0,0 +1,32 @@
+<?php
+
+$frameworks = array("CodeIgniter","Zend Framework","Cake PHP","Kohana","Vishal","PHP","Java") ;
+
+$name = $_GET["name"];
+
+if (strlen($name) > 0) {
+
+    $match = "";
+
+    for ($i = 0; $i < count($frameworks); $i++) {
+
+        if (strtolower($name) == strtolower(substr($frameworks[$i], 0, strlen($name)))) {
+
+            if ($match == "") {
+
+                $match = $frameworks[$i];
+
+            } else {
+
+                $match = $match . " , " . $frameworks[$i];
+
+            }
+
+        }
+
+    }
+
+}
+
+echo ($match == "") ? 'no match found' : $match;
+?>
